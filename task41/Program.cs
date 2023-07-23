e5ac64e05222293882d25fcaee1d5293dcccf197
AleksandrBK <lebedevavlv@gmail.com>
2023-07-23 14:41:14 +0400

    Task 41

--- task41/Program.cs
+++ task41/Program.cs
@@ -4,3 +4,35 @@
 // 1, -7, 567, 89, 223-> 3
 
 
+int[] GetArray()
+{
+    Console.WriteLine("Введите числа, разделяя их пробелами:");
+    string input = Console.ReadLine();
+    string[] numbersAsStrings = input.Split(' ');
+    int[] array = new int[numbersAsStrings.Length];
+
+    for (int i = 0; i < numbersAsStrings.Length; i++)
+    {
+        array[i] = int.Parse(numbersAsStrings[i]);
+    }
+
+    return array;
+}
+
+int CheckArray(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count ++;
+        }
+    }
+    return count;
+}
+
+int[] array = GetArray();
+int count = CheckArray(array);
+Console.WriteLine($"В массиве: [ {string.Join("; ", array)} ] количество чисел больше нуля равно: {count}");
+
